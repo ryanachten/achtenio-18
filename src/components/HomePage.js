a79a8a8fc95d38d5dbc533daba589c2924747619
Ryan Achten <ryanachten@gmail.com>
2018-01-12 16:21:21 +1300

    Add texture offset effect to main obj

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,33 +15,29 @@ class HomePage extends React.Component {
     const height = this.mount.clientHeight;
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      width / height,
-      0.1,
-      1000
-    );
+    const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
     const renderer = new THREE.WebGLRenderer({ antialias: true })
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const geometry = new THREE.OctahedronGeometry(0, 1)
     const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
-    const cube = new THREE.Mesh(geometry, material)
+    const imgMesh = new THREE.Mesh(geometry, material)
 
     camera.position.z = 4
-    scene.add(cube)
-    renderer.setClearColor('#000000')
+    scene.add(imgMesh)
+    renderer.setClearColor('#FFFCF9')
     renderer.setSize(width, height)
 
     var texture = new THREE.TextureLoader().load( "./img/textures/test1.png", (texture) => {
       material.map = texture
-      texture.wrapS = THREE.RepeatWrapping;
-      texture.wrapT = THREE.RepeatWrapping;
+      texture.wrapS = THREE.MirroredRepeatWrapping;
+      texture.wrapT = THREE.MirroredRepeatWrapping;
       texture.repeat.set( 4, 4 );
 
       this.scene = scene
       this.camera = camera
       this.renderer = renderer
       this.material = material
-      this.cube = cube
+      this.imgMesh = imgMesh
+      this.texture = texture
 
       this.mount.appendChild(this.renderer.domElement)
       this.start()
@@ -67,8 +63,11 @@ class HomePage extends React.Component {
   }
 
   animate() {
-    this.cube.rotation.x += 0.01
-    this.cube.rotation.y += 0.01
+    this.imgMesh.rotation.x += 0.01
+    this.imgMesh.rotation.y += 0.01
+
+    this.texture.offset.x -= 0.005
+    this.texture.offset.y -= 0.005
 
     this.renderScene()
     this.frameId = window.requestAnimationFrame(this.animate)
