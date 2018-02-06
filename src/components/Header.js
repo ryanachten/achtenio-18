f423413e3c6c99f1a2f393f119c2b1684f5596d9
Ryan Achten <ryanachten@gmail.com>
2018-02-06 19:23:45 +1300

    Add initial topnav styles and functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import $ from 'jquery';
 
 const Header = () => (
   <header className="topnav--container">
     <NavLink className="logotype topnav--link" to="/" activeClassName="is-active" exact={true}>r/a</NavLink>
+    <button className="topnav--menubutton" onClick={toggleNav}></button>
     <div className="topnav--linkcontainer">
       <NavLink className="topnav--link" to="/work" activeClassName="is-active">Work</NavLink>
       <NavLink className="topnav--link" to="/work" activeClassName="is-active">CV</NavLink>
       <NavLink className="topnav--link" to="/contact" activeClassName="is-active">Contact</NavLink>
     </div>
-    <button className="topnav--open"></button>
+
   </header>
 );
+
+const toggleNav = () => {
+  $('.topnav--linkcontainer').toggleClass('topnav--open');
+  $('.topnav--menubutton').toggleClass('topnav--open');
+};
 
 export default Header;
