fcc57e0e31e70faf94965c7105c0fa47adc396ff
Ryan Achten <ryanachten@gmail.com>
2018-04-09 13:27:13 +1200

    Extract css from production bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
 
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: "./src/app.js",
@@ -17,11 +19,21 @@ module.exports = (env) => {
         exclude: /node_modules/
       },{
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },{
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        }),
       },{
         test: /\.svg/,
         use: {
@@ -30,6 +42,9 @@ module.exports = (env) => {
         }
       }]
     },
+    plugins: [
+      CSSExtract
+    ],
     devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
