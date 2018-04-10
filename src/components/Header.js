280d9d8dac4507b21e694bc25ce1beea81499d1d
Ryan Achten <ryanachten@gmail.com>
2018-04-11 10:21:00 +1200

    Update Header nav RWD styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,13 @@ import $ from 'jquery';
 const Header = () => (
   <header className="topNav--container">
     <NavLink className="logotype topNav--link" to="/" activeClassName="is-active" exact={true}>r/a</NavLink>
-    <button className="topNav--menuButton" onClick={toggleNav}></button>
     <div className="topNav--linkContainer">
       <NavLink className="topNav--link" to="/work" activeClassName="is-active">Work</NavLink>
       <NavLink className="topNav--link" to="/cv" activeClassName="is-active">CV</NavLink>
       <NavLink className="topNav--link" to="/contact" activeClassName="is-active">Contact</NavLink>
     </div>
+    <button className="topNav--menuButton" onClick={toggleNav}></button>
+
 
   </header>
 );
