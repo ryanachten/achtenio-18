c79265e9f660372c3d8cd9c37b1409b86f3fa03f
Ryan Achten <ryanachten@gmail.com>
2018-04-12 16:52:54 +1200

    Update about page styles and content

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,12 +2,29 @@ import React from 'react';
 
 const AboutPage = () => (
   <div className="about__container">
-    Ryan Achten. I’m a designer & developer based in Wellington, New Zealand. I’ve created all sorts of digital media, encompassing graphic, motion, and interactive works. I’ve worked with amazing people from cultural institutions, academia, musicians and other creative types.<br />
-    <br />
-    Interested in working together?<br />
-    Get in touch at ryanachten (at) gmail (dot) com<br />
-    <br />
-    For more of my work, take a peak at the social sites below:
+    <img className="about__logo" src="/img/icons/logo.svg"></img>
+    <p className="about__splashText">
+      My name is Ryan Achten.<br/>
+      I’m a designer & developer based in Wellington, New Zealand.<br/>
+      <br/>
+      I’ve created all sorts of digital media, encompassing graphic, motion, and interactive works. I’ve worked with amazing people from cultural institutions and academics to musicians and other creative types.<br/>
+    </p>
+    <p>
+      <br/>
+      Interested in working together? Get in touch at ryanachten (at) gmail (dot) com<br/>
+      <br/>
+      For more of my work, take a peak at the social sites below:
+    </p>
+    <ul className="about__socialLinks">
+      <li><a href="https://www.behance.net/ryanachten">
+        <img src="/img/icons/social/behance.svg"></img></a></li>
+      <li><a href="https://github.com/ryanachten">
+        <img src="/img/icons/social/github.svg"></img></a></li>
+      <li><a href="https://www.linkedin.com/in/ryanachten/">
+        <img src="/img/icons/social/vimeo.svg"></img></a></li>
+      <li><a href="https://vimeo.com/ryanachten">
+        <img src="/img/icons/social/linkedin.svg"></img></a></li>
+    </ul>
   </div>
 );
 
