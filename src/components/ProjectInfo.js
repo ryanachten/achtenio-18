dcab7512c0e3793ffae98667a9c485f6a0cf3155
Ryan Achten <ryanachten@gmail.com>
2018-04-13 16:17:21 +1200

    Replace img:hover 3d rotate effect with svg turbulance effect

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -1,81 +1,138 @@
 import React from 'react';
 import uuid from 'uuid';
+import $ from 'jquery';
 
-const ProjectInfo = (props) => {
+class ProjectInfo extends React.Component{
 
-  const {
-    title,
-    subtitle,
-    date,
-    description,
-    headerImg,
-    roles,
-    credits = undefined,
-    tools,
-    social,
-    textureImg,
-    images
-  } = props.project;
+  constructor(props){
+    super(props);
 
-  // const rotateImg = () => {
-  //   const randX = Math.floor(Math.random()*2);
-  //   const randY = Math.floor(Math.random()*2);
-  //   const randZ = Math.floor(Math.random()*2);
-  //   const randDeg = Math.floor(Math.random()*5+1);
-  //   return(
-  //     { transform: `rotate3d(${randX}, ${randY}, ${randZ}, ${randDeg}deg)` }
-  //   );
-  // };
+    this.startFilter = this.startFilter.bind(this);
+    this.cancelFilter = this.cancelFilter.bind(this);
+    this.updateFilter = this.updateFilter.bind(this);
+  }
 
-  return(
-    <div>
-      <section className="project__splashContainer">
-        <div className="project__splashbox">
-          <div><h1>{title}</h1></div>
-          <div><h2>{subtitle}</h2></div>
-          <div><h2>{date}</h2></div>
-        </div>
-      </section>
+  startFilter({target}){
+    $(target).addClass('filter');
+    this.updateFilter();
+  };
 
-      <div className="project__infoContainer">
+  updateFilter(){
+    this.frameId = window.requestAnimationFrame(this.updateFilter);
+    // const turb = $('feTurbulence')[0];
+    // const displace = $('feDisplacementMap')[0].scale;
+    // console.log(displace);
+    $('feDisplacementMap')[0].scale.baseVal += 0.5;
+    // $('feTurbulence')[0].baseFrequencyX.baseVal += 0.01;
+    // console.log(displace);
+  }
 
-        <section className="project__section overview">
-          <h2 className="project__sectionHeader">Overview</h2>
-          <p>{description}</p>
+  cancelFilter({target}){
+    $(target).removeClass('filter');
+    window.cancelAnimationFrame(this.frameId);
+    $('feDisplacementMap')[0].scale.baseVal = 0;
+  }
+
+  render = () => {
+
+    const {
+      title,
+      subtitle,
+      date,
+      description,
+      headerImg,
+      roles,
+      credits = undefined,
+      tools,
+      social,
+      textureImg,
+      images
+    } = this.props.project;
+
+    return(
+      <div>
+        <svg>
+          <defs>
+            <filter id="svgFilter">
+              <feTurbulence
+                type="turbulence" baseFrequency="0.01"
+                numOctaves="1" result="turbulence"/>
+              <feDisplacementMap
+                in="SourceGraphic" in2="TURBULENCE" scale="0" />
+            </filter>
+          </defs>
+        </svg>
+        <section className="project__splashContainer">
+          <div className="project__splashbox">
+            <div><h1>{title}</h1></div>
+            <div><h2>{subtitle}</h2></div>
+            <div><h2>{date}</h2></div>
+          </div>
         </section>
 
-        <hr className="project__divider" />
+        <div className="project__infoContainer">
 
-        <section className="project__section">
-          <h2 className="project__sectionHeader">Role</h2>
-          <ul>
-            {roles.map( (role)=>(
-              <li key={uuid()}>{role}</li>
-            ))}
-          </ul>
-        </section>
-        { credits && (
+          <section className="project__section overview">
+            <h2 className="project__sectionHeader">Overview</h2>
+            <p>{description}</p>
+          </section>
+
+          <hr className="project__divider" />
+
           <section className="project__section">
-            <h2 className="project__sectionHeader">Credits</h2>
+            <h2 className="project__sectionHeader">Role</h2>
             <ul>
-              { credits.map( (credit) => (
-                <li key={uuid()}>{credit.name} ~ {credit.role}</li>
-              )) }
+              {roles.map( (role)=>(
+                <li key={uuid()}>{role}</li>
+              ))}
             </ul>
           </section>
-        )}
+          { credits && (
+            <section className="project__section">
+              <h2 className="project__sectionHeader">Credits</h2>
+              <ul>
+                { credits.map( (credit) => (
+                  <li key={uuid()}>{credit.name} ~ {credit.role}</li>
+                )) }
+              </ul>
+            </section>
+          )}
+
+          <section className="project__section">
+            <h2 className="project__sectionHeader">Tools</h2>
+            <ul>
+              {tools.map( (tool)=>(
+                <li key={uuid()}>{tool}</li>
+              ))}
+            </ul>
+          </section>
+
+          <section className="project__section social">
+            <h2 className="project__sectionHeader">Docs</h2>
+            <ul>
+              {social.map( (site)=>(
+                <li className="project__socialItem" key={uuid()}>
+                  <a className={`project__socialIcon ${site.host.toLowerCase()}`} href={site.url}></a>
+                </li>
+              ))}
+            </ul>
+          </section>
+
+        </div>
+
+
+        { images.map( (img) => (
+
+          <img key={uuid()} className="project__img" src={img}
+            onMouseEnter={this.startFilter}
+            onMouseLeave={this.cancelFilter}
+            ></img>
+
+        ))}
 
         <section className="project__section">
-          <h2 className="project__sectionHeader">Tools</h2>
-          <ul>
-            {tools.map( (tool)=>(
-              <li key={uuid()}>{tool}</li>
-            ))}
-          </ul>
-        </section>
-
-        <section className="project__section social">
-          <h2 className="project__sectionHeader">Docs</h2>
+          <h2 className="project__sectionHeader">Further Info</h2>
+          <p>Interested? See further documentation at the links below:</p>
           <ul>
             {social.map( (site)=>(
               <li className="project__socialItem" key={uuid()}>
@@ -84,34 +141,9 @@ const ProjectInfo = (props) => {
             ))}
           </ul>
         </section>
-
       </div>
-
-
-      { images.map( (img) => (
-
-        <img key={uuid()} className="project__img" src={img}
-          // onMouseOver={({target: {style}}) => {
-          //   if (style.animationPlayState !== 'initial') {
-          //     style.animation = 'img-roll 3s';
-          //   }}}
-          ></img>
-
-      ))}
-
-      <section className="project__section">
-        <h2 className="project__sectionHeader">Further Info</h2>
-        <p>Interested? See further documentation at the links below:</p>
-        <ul>
-          {social.map( (site)=>(
-            <li className="project__socialItem" key={uuid()}>
-              <a className={`project__socialIcon ${site.host.toLowerCase()}`} href={site.url}></a>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </div>
-  );
+    );
+  }
 };
 
 export default ProjectInfo;
