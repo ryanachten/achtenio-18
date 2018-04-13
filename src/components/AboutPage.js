dd25f8ab7cc9a59c02841c21fe00e57ba7548955
Ryan Achten <ryanachten@gmail.com>
2018-04-13 16:16:51 +1200

    Simplify about page, add mail icon

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -9,12 +9,6 @@ const AboutPage = () => (
       <br/>
       Works with digital media, encompassing graphic, motion, and interactive projects. Has collaborated with cultural institutions, academics, musicians and other creative types.<br/>
     </p>
-    <p>
-      <br/>
-      Interested in working together? Get in touch at ryanachten (at) gmail (dot) com<br/>
-      <br/>
-      For more of my work, take a peak at the social sites below:
-    </p>
     <ul className="about__socialLinks">
       <li><a href="https://www.behance.net/ryanachten">
         <img src="/img/icons/social/behance.svg"></img></a></li>
@@ -24,6 +18,8 @@ const AboutPage = () => (
         <img src="/img/icons/social/vimeo.svg"></img></a></li>
       <li><a href="https://vimeo.com/ryanachten">
         <img src="/img/icons/social/linkedin.svg"></img></a></li>
+      <li><a href="mailto:ryanachten@gmail.com">
+        <img src="/img/icons/social/mail.svg"></img></a></li>
     </ul>
   </div>
 );
