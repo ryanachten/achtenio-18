d6f75ecb807c6b83555f20d7e73976602befecbd
Ryan Achten <ryanachten@gmail.com>
2018-04-24 11:27:31 +1200

    set renderer dom element as display none by default (fade in after asset load)

--- src/three/initScene.js
+++ src/three/initScene.js
@@ -36,5 +36,7 @@ export const renderer = (width, height) => {
   const render = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   render.setClearColor(0x000000, 0); //'#FFF7F0'
   render.setSize(width, height);
+  render.domElement.id = 'three-canvas';
+  render.domElement.style.display = 'none';
   return render;
 }
